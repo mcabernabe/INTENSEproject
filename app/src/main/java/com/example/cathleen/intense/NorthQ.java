a0d3d6c658b93f59bb0c01753ba6a24a3e731556
cathleen <mcathleenbernabe@gmail.com>
2018-11-13 22:51:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cathleen/intense/NorthQ.java
@@ -0,0 +1,13 @@
+package com.example.cathleen.intense;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NorthQ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_north_q);
+    }
+}
---
+++ app/src/main/java/com/example/cathleen/intense/SouthQ.java
@@ -0,0 +1,13 @@
+package com.example.cathleen.intense;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SouthQ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_south_q);
+    }
+}
